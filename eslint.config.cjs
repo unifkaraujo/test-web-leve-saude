9d6df691fc80c4b7aaa95d75e25b19e763445e85
Kaique Araujo <kaique.earaujo@outlook.com>
2025-07-23 17:46:13 -0300

    chore: configura ESLint e Prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const js = require('@eslint/js');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const reactPlugin = require('eslint-plugin-react');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
